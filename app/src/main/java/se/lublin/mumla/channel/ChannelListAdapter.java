quite/mumla

commit af8788258e10380bc4464161f485256361e5d1ad

Remove cruft from animations that never worked

app/src/main/java/se/lublin/mumla/channel/ChannelListAdapter.java
@@ -326,23 +326,6 @@ public class ChannelListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         }
     }
 
-    /**
-     * Updates the user's state icon with a nice animation.
-     * @param user The user to update.
-     * @param view The view containing this adapter.
-     */
-    public void animateUserStateUpdate(IUser user, RecyclerView view) {
-        long itemId = user.getSession() | USER_ID_MASK;
-        UserViewHolder uvh = (UserViewHolder) view.findViewHolderForItemId(itemId);
-        if (uvh != null) {
-            Drawable newState = getTalkStateDrawable(user);
-            if (!uvh.mUserTalkHighlight.getDrawable().getCurrent().getConstantState()
-                    .equals(newState.getConstantState())) {
-                uvh.mUserTalkHighlight.setImageDrawable(newState);
-            }
-        }
-    }
-
     private Drawable getTalkStateDrawable(IUser user) {
         Resources resources = mContext.getResources();
         if (user.isSelfDeafened()) {

app/src/main/java/se/lublin/mumla/channel/ChannelListFragment.java
@@ -131,14 +131,9 @@ public class ChannelListFragment extends HumlaServiceFragment implements OnChann
 
         @Override
         public void onUserStateUpdated(IUser user) {
-            mChannelListAdapter.animateUserStateUpdate(user, mChannelView);
             getActivity().supportInvalidateOptionsMenu(); // Update self mute/deafen state
         }
 
-        @Override
-        public void onUserTalkStateUpdated(IUser user) {
-            mChannelListAdapter.animateUserStateUpdate(user, mChannelView);
-        }
     };
 
     private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {

app/src/main/java/se/lublin/mumla/channel/ChannelFragment.java
@@ -17,7 +17,6 @@
 
 package se.lublin.mumla.channel;
 
-import android.animation.Animator;
 import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.Bundle;
@@ -329,33 +328,7 @@ public class ChannelFragment extends HumlaServiceFragment implements SharedPrefe
     }
 
     private void setTalkButtonHidden(final boolean hidden) {
-        if (hidden ^ mTalkButtonHidden) {
-            Settings settings = Settings.getInstance(getActivity());
-            mTalkView.animate()
-                    .setDuration(300)
-                    .translationY(hidden ? settings.getPTTButtonHeight() : 0)
-                    .setListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animation) {
-                            mTalkView.setVisibility(View.VISIBLE);
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mTalkView.setVisibility(hidden ? View.GONE : View.VISIBLE);
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animation) {
-
-                        }
-                    });
-        }
+        mTalkView.setVisibility(hidden ? View.GONE : View.VISIBLE);
         mTalkButtonHidden = hidden;
     }
 

commit 0d149c1a87e07b0aebfed439325ae926c5466b54

Bring back better update of user's state icon
Was broken in af878825

app/src/main/java/se/lublin/mumla/channel/ChannelListAdapter.java
@@ -23,6 +23,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Drawable.ConstantState;
 import android.os.RemoteException;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -323,6 +324,23 @@ public class ChannelListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             }
         } catch (IllegalStateException e) {
             Log.d(TAG, "exception in updateChannels: " + e);
+        }
+    }
+
+    /**
+     * Update a user's state icon
+     * @param user The user to update.
+     * @param view The view containing this adapter.
+     */
+    public void updateUserStates(IUser user, RecyclerView view) {
+        long itemId = user.getSession() | USER_ID_MASK;
+        UserViewHolder uvh = (UserViewHolder) view.findViewHolderForItemId(itemId);
+        if (uvh != null) {
+            Drawable newState = getTalkStateDrawable(user);
+            ConstantState state = uvh.mUserTalkHighlight.getDrawable().getCurrent().getConstantState();
+            if (state != null && !state.equals(newState.getConstantState())) {
+                uvh.mUserTalkHighlight.setImageDrawable(newState);
+            }
         }
     }
 

app/src/main/java/se/lublin/mumla/channel/ChannelListFragment.java
@@ -131,9 +131,14 @@ public class ChannelListFragment extends HumlaServiceFragment implements OnChann
 
         @Override
         public void onUserStateUpdated(IUser user) {
+            mChannelListAdapter.updateUserStates(user, mChannelView);
             getActivity().supportInvalidateOptionsMenu(); // Update self mute/deafen state
         }
 
+        @Override
+        public void onUserTalkStateUpdated(IUser user) {
+            mChannelListAdapter.updateUserStates(user, mChannelView);
+        }
     };
 
     private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {