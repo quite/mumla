6e16a1c4933dfb6d3391330507e25fce5ed699bc
Daniel Lublin <daniel@lublin.se>
2026-02-06 14:46:27 +0100

    Add strings for next release

--- app/src/main/java/se/lublin/mumla/app/DialogUtils.java
+++ app/src/main/java/se/lublin/mumla/app/DialogUtils.java
@@ -34,6 +34,7 @@ public final class DialogUtils {
         // These *must* be added in ascending semver order (1.2.3, 1.10.1, ...)
         NEWS_ITEMS.put("3.7.0", R.string.app_news_items_v3_7_0);
         NEWS_ITEMS.put("3.7.1", R.string.app_news_items_v3_7_1);
+        NEWS_ITEMS.put("3.7.2", R.string.app_news_items_v3_7_2);
         RELEVANT_VERSIONS = new ArrayList<>();
         for (String candidate : NEWS_ITEMS.keySet()) {
             // Relevant as long as candidate-version <= build-version; otherwise break
